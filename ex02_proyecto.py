4992429ff0c6eba52b742fdd890493f538eae72f
JdD <Jorge@Jorges-MacBook-Pro-13.local>
2023-07-06 20:22:29 +0200

    Add description line 5

--- ex02_proyecto.py
+++ ex02_proyecto.py
@@ -2,5 +2,5 @@
 
 nombre = input("Nombre de empleado:")
 ventas = int(input("¿Cuantas ventas has hecho hoy?")) #al guardarse como string lo transformo a int
-comisiones = round((ventas * 13)/ 100, 2) #comisión del 13%
+comisiones = round((ventas * 13)/ 100, 2) #comisión del 13% usando round para redondear a 2 decimales
 print(f"{nombre} hoy recibes {comisiones} euros por tus ventas.")
