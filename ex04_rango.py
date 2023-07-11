16e8a8fae6d6bc0bea7a4367247c94d73f169dc0
jorgedediosb <jorgededios@icloud.com>
2023-07-11 19:05:28 +0200

    add ex04_rango.py

---
+++ ex04_rango.py
@@ -0,0 +1,17 @@
+#Ejercicios lección 4 - RANGO
+
+lista = [1,2,3,4]
+for numeros in lista:
+    print(numeros)
+
+for numeros in range(5):
+    print(numeros)
+
+for numeros in range(20, 31):
+    print(numeros)
+
+for numeros in range(20, 31, 2):
+    print(numeros)
+
+list = list(range(0,101)) #forma sencilla de imprimir una lista extensa
+print(list)
