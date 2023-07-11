4c1aaf2c21b7488889f2405ec2051eee60122b51
jorgedediosb <jorgededios@icloud.com>
2023-07-11 19:41:24 +0200

    modified ex04_rango.py

--- ex04_rango.py
+++ ex04_rango.py
@@ -13,5 +13,16 @@ for numeros in range(20, 31):
 for numeros in range(20, 31, 2):
     print(numeros)
 
-list = list(range(0,101)) #forma sencilla de imprimir una lista extensa
-print(list)
+#forma sencilla de imprimir una lista extensa usando range
+lista1 = list(range(0,101))
+print(lista1)
+
+#lista con todos los nºs múltiplos de 3, del 3 al 300 
+lista2 = list(range(3,301,3))
+print(lista2)
+
+# Utiliza la función range() y un loop para sumar los cuadrados de todos los números del 1 al 15 (inclusive).
+suma_cuadrados = 0
+for numeros in range(1,16):
+    suma_cuadrados = suma_cuadrados + numeros**2
+print(suma_cuadrados)
