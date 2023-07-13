82c3257b77c61144070a9d1c22805a00c9e08cf4
jorgedediosb <jorgededios@icloud.com>
2023-07-13 11:17:32 +0200

    add ex05_metodos.py

---
+++ ex05_metodos.py
@@ -0,0 +1,15 @@
+# Ejercicios lección 5 - MÉTODOS
+
+# Existen decenas de métodos/funciones. Documentación en python.orgs
+dic = {"clave1":"valor1", 'clace2':'valor2'}
+print(dic)
+dic.popitem()
+print(dic)
+dic.clear()
+print(dic)
+
+print(",:_#,,,,,,:::____##Pyt%on_ _Total,,,,,,::#".lstrip(',:%_#'))
+
+frutas = ["mango", "banana", "cereza", "ciruela", "pomelo"]
+frutas.insert(cereza,naranja)
+print(frutas)
