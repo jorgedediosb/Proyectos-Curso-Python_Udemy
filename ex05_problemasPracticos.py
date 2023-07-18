a6289e01fa12a2b6c4e5a229ff921f77d31fe760
jorgedediosb <jorgededios@icloud.com>
2023-07-18 18:36:24 +0200

    update ex05_problemasPracticos.py

--- ex05_problemasPracticos.py
+++ ex05_problemasPracticos.py
@@ -7,12 +7,15 @@
 # Si la suma de los 3 números es un valor entre 10 y 15 (incluidos) va a devolver el número de valor intermedio
 
 def devolver_distintos(num1, num2, num3):
-    if num1 + num2 + num3 > 15:
-        return max(num1, num2, num3)
-    elif num1 + num2 + num3 < 10:
-        return min(num1, num2, num3)
+    suma = num1 + num2 + num3
+    lista = [num1,num2,num3]
+    if suma > 15:
+        return max(lista)
+    elif suma < 10:
+        return min(lista)
     else:
-        return (num1, num2, num3)[1] #[1] es el nº intermedios de los 3 (posición 2) 
+        lista.sort() #lista ordenada
+        return (lista)[1] #[1] es el nº intermedios de los 3 (posición 2) 
 
 print(devolver_distintos(1,2,4))
 
@@ -23,7 +26,7 @@ print(devolver_distintos(1,2,4))
 # Al invocar la función pasamos la palabra "entretenido", debería devolver ['d', 'e', 'i', 'n', 'o', 'r', 't']
 
 def letras_unicas(palabra):
-    letras = list(set(palabra))
+    letras = list(set(palabra)) #set da elementos únicos e irrepetibles
     letras.sort()
     return letras
 
@@ -43,8 +46,9 @@ def dos_ceros(*args):
     
 print(dos_ceros(5, 6, 1, 3, 0, 9, 3, 5))
 
-# Función que requiera un solo argumento numérico.
-# Mostrar en pantalla todos los números primos existentes en el rango que va desde cero hasta ese número incluido,
+
+# Problema 4
+# Función con un argumento que  muestre todos los números primos existentes en el rango que va desde cero hasta ese número incluido,
 # y va a devolver la cantidad de números primos que encontró.
 # Por convención el 0 y el 1 no se consideran primos.
 
@@ -52,15 +56,15 @@ def contar_primos(num):
     primos = []
     for num in range(2, num + 1): #nºs a partir de 2
         es_primo = True
-        for i in range(2, int(num ** 0.5) + 1): #verificar si "num" es divisible por algún número entre 2 y su raíz cuadrada
+        for i in range(2, int(num ** 0.5) + 1): #verificar si "num" es divisible por algún número entre 2 y su raíz cuadrada para evitar que lo calcule según el resto de nºs.
             if num % i == 0: # si "num" es divisible por "i" no es primo.
                 es_primo = False
                 break
-            if es_primo:
-                primos.append(num) #si es primo se agrega a la lista
-                print(num)
+        if es_primo:
+            primos.append(num) #si es primo se agrega a la lista
+            print(num)
     cantidad_primos = len(primos)
     return cantidad_primos
 
 cantidad = contar_primos(20)
-print("Cantidad de números primos encontrados:", cantidad)
+print("Cantidad nºs primos encontrados:", cantidad)
