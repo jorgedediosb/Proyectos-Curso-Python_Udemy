8f4791821985a4b958d2b3885e7deaabe628a746
jorgedediosb <jorgededios@icloud.com>
2023-07-18 19:13:40 +0200

    update ex05_proyectoFinal_ahorcado.py

--- ex05_proyectoFinal_ahorcado.py
+++ ex05_proyectoFinal_ahorcado.py
@@ -1,57 +1,92 @@
 # Proyecto Final lección 5 - Juego "Ahorcado"
 
-import random
+from random import choice
 
-def elegir_palabra(lista_palabras):
-    palabra = random.choice(lista_palabras)
-    return palabra
-lista_palabras = ["python", "programacion", "ordenador", "teclado", "raton"]
-palabra_secreta = elegir_palabra(lista_palabras)
-print(palabra_secreta)
+palabras = ['panadero', 'dinosaurio', 'helipuerto', 'tiburon']
+letras_correctas = []
+letras_incorrectas = []
+intentos = 6
+aciertos = 0
+juego_terminado = False
 
-def mostrar_palabra(palabra, letras_adivinadas):
-    palabra_mostrada = ""
-    for letra in palabra:
-        if letra in letras_adivinadas:
-            palabra_mostrada += letra
-        else:
-            palabra_mostrada += "_"
-    return palabra_mostrada
-letras_adivinadas = ["o", "r", "n", "a"]
-palabra_mostrada = mostrar_palabra(palabra_secreta, letras_adivinadas)
-print(palabra_mostrada)
+def elegir_palbra(lista_palabras):
+    palabra_elegida = choice(lista_palabras)
+    letras_unicas = len(set(palabra_elegida))
+
+    return palabra_elegida, letras_unicas
 
 def pedir_letra():
-    letra = input("Ingresa una letra: ")
-    while not letra.isalpha():
-        letra = input("Ingresa una letra válida: ")
-    return letra.lower()
-letra_ingresada = pedir_letra()
-print(letra_ingresada)
+    letra_elegida = ''
+    es_valida = False
+    abecedario = 'abcdefghijklmnñopqrstuvwxyz'
 
-def verificar_letra(letra, palabra, letras_adivinadas):
-    if letra in palabra and letra not in letras_adivinadas:
-        return True
+    while not es_valida:
+        letra_elegida = input("Elige una letra: ")
+        if letra_elegida in abecedario and len(letra_elegida) == 1:
+            es_valida = True
+        else:
+            print("No has elegido una letra correcta")
+
+    return letra_elegida
+
+def mostrar_nuevo_tablero(palabra_elegida):
+
+    lista_oculta = []
+
+    for l in palabra_elegida:
+        if l in letras_correctas:
+            lista_oculta.append(l)
+        else:
+            lista_oculta.append('-')
+
+    print(' '.join(lista_oculta))
+
+def chequear_letra(letra_elegida, palabra_oculta, vidas, coincidencias):
+
+    fin = False
+
+    if letra_elegida in palabra_oculta:
+        letras_correctas.append(letra_elegida)
+        coincidencias += 1
     else:
-        return False
-letra = "o"
-letras_adivinadas = ["o", "r", "n", "a"]
-es_letra_valida = verificar_letra(letra, palabra_secreta, letras_adivinadas)
-print(es_letra_valida)
+        letras_incorrectas.append(letra_elegida)
+        vidas -= 1
 
-def verificar_letra(letra, palabra, letras_adivinadas):
-    if letra in palabra and letra not in letras_adivinadas:
-        return True
-    else:
-        return False
-letra = "o"
-letras_adivinadas = ["o", "r", "n", "a"]
-es_letra_valida = verificar_letra(letra, palabra_secreta, letras_adivinadas)
-print(es_letra_valida)
+    if vidas == 0:
+        fin = perder()
+    elif coincidencias == letras_unicas:
+        fin = ganar(palabra_oculta)
 
-lista_palabras = ["python", "programacion", "ordenador", "teclado", "raton"]
-palabra_secreta = random.choice(lista_palabras)
-jugar(palabra_secreta)
+    return vidas, fin, coincidencias
+
+def perder():
+    print("Te has quedado sin vidas")
+    print("La palabra oculta era " + palabra)
+
+    return True
+
+def ganar(palabra_descubierta):
+    mostrar_nuevo_tablero(palabra_descubierta)
+    print("Felicitaciones, has encontrado la palabra!!!")
+
+    return True
+
+palabra, letras_unicas = elegir_palbra(palabras)
+
+while not juego_terminado:
+    print('\n' + '*' * 40)
+    print("¡Bienvenido al juego del ahorcado!")
+    print('*' * 40 + '\n' + '\n')
+    mostrar_nuevo_tablero(palabra)
+    print('\n')
+    print('Letras incorrectas: ' + '-'.join(letras_incorrectas))
+    print(f'Vidas: {intentos}')
+    print('\n' + '*' * 40 + '\n')
+    letra = pedir_letra()
+
+    intentos, terminado, aciertos = chequear_letra(letra,palabra,intentos,aciertos)
+
+    juego_terminado = terminado
 
 # Para que se elijan palabras al azar sin tener que meterlas:
 # 1º Crear base de datos en archivo de texto con las palabras (cada una en una línea).
