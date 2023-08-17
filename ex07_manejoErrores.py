162336d7ebed5eee437c8fab3e955863b118a980
jorgedediosb <jorgededios@icloud.com>
2023-08-17 11:27:33 +0200

    update ex07_manejoErrores.py

--- ex07_manejoErrores.py
+++ ex07_manejoErrores.py
@@ -14,3 +14,34 @@ def pedirNumero():
     print("Gracias")
 
 pedirNumero()
+
+
+def suma(num1,num2):
+    try:
+        print(num1+num2)
+    except:
+        print("Error inesperado")
+
+
+def cociente(num1,num2):
+    try:
+        print(num1/num2)
+#MENSAJE EN PANTALLA
+    except TypeError:
+        print("Los argumentos a ingresar deben ser números")
+    except ZeroDivisionError:
+        print("El segundo argumento no debe ser cero")
+
+
+def abrir_archivo(nombre_archivo):
+    try:
+        archivo = open(nombre_archivo)
+#MENSAJES EN PANTALLA:
+    except FileNotFoundError:
+        print("El archivo no fue encontrado")
+    except:
+        print("Error desconocido")
+    else:
+        print("Abriendo exitosamente")
+    finally:
+        print("Finalizando ejecución")
