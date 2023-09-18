f9be2de205038f463861bd3c889907c9664eb33e
jorgedediosb <jorgededios@icloud.com>
2023-09-18 13:54:18 +0200

    add p01

---
+++ proyectos/p01_nombresCerveza/p01_nombresCerveza.py
@@ -0,0 +1,4 @@
+# PROYECTO "NOMBRES DE CERVEZAS"
+# El programa crea un nombre para una cerveza a partir de las respuestas a las preguntas del programa
+
+print("El nombre de tu cerveza\nes '" + input("Que ciudad te gustaria visitar?: ") + " " + input("Cual es tu color favorito?: ") + "'\nFelicitaciones!")
