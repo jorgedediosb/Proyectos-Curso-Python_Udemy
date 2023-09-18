a5134f79879607e46fb1055cec2f4a6fa0b71712
jorgedediosb <jorgededios@icloud.com>
2023-09-18 13:06:13 +0200

    añadido archivo a ejercicios

---
+++ ejercicios/ex01_print.py
@@ -0,0 +1,16 @@
+#Ejercicios curso Python Udemy - Lección 1 - print y input
+
+print('Estudiar con "Python Total" es genial')
+
+print('Línea 1\nLínea 2\nLínea 3')
+
+print('A\tB\tC\nD\tE\tF\nG\tH\tI')
+
+print('\tBarra Normal: /\n\tBarra Invertida: \\')
+
+print(input("¿Qué estás estudiando?"))
+
+print("Tu nombre es: " + input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: "))
+
+#proyecto lección 1 "Nombre de cerveza":
+print("Tu nombre de cerveza es:\n'" + input("Escribe un nombre de mujer: ") + " " + input("Escribe un adjetivo hermoso: ") + "'")
