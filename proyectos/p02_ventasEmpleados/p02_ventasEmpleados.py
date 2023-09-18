e66d7b0ca5460a2954cdd73541a015f106fa4c80
jorgedediosb <jorgededios@icloud.com>
2023-09-18 18:19:27 +0200

    git commit -a "changes in p02"

--- proyectos/p02_ventasEmpleados/p02_ventasEmpleados.py
+++ proyectos/p02_ventasEmpleados/p02_ventasEmpleados.py
@@ -1,6 +1,8 @@
-#Programa que calcula el 13% de las ventas de los empleados
+# Programa que calcula el 13% de las ventas de los empleados
 
-nombre = input("Nombre de empleado:")
-ventas = int(input("¿Cuantas ventas has hecho hoy?")) #al guardarse como string lo transformo a int
-comisiones = round((ventas * 13)/ 100, 2) #comisión del 13% usando round para redondear a 2 decimales
-print(f"{nombre} hoy recibes {comisiones} euros por tus ventas.")
+nombre = input("Nombre de empleado: ")
+#al guardarse como string debemos transformar a int
+ventas = int(input("¿Cuantas ventas has hecho hoy? "))
+#comisión del 13% usando round para redondear a 2 decimales
+comisiones = round((ventas * 13)/ 100, 2)
+print(f"{nombre}, hoy recibes {comisiones} euros por tus ventas.")
