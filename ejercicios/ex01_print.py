fdf15bfe1c73fc5c4c149c1304ba8cf0cd61a62e
jorgedediosb <jorgededios@icloud.com>
2023-10-18 19:32:39 +0200

    update ex01 y ex02

--- ejercicios/ex01_print.py
+++ ejercicios/ex01_print.py
@@ -1,16 +1,18 @@
-#Ejercicios curso Python Udemy - Lección 1 - print y input
+# Ejercicios curso Python Udemy - Lección 1 - print y input
 
+# Uso de print para imprimir texto:
 print('Estudiar con "Python Total" es genial')
 
+# Uso de print con saltos de línea (\n):
 print('Línea 1\nLínea 2\nLínea 3')
 
+# Uso de print con tabulaciones (\t) y saltos de línea (\n):
 print('A\tB\tC\nD\tE\tF\nG\tH\tI')
 
 print('\tBarra Normal: /\n\tBarra Invertida: \\')
 
-print(input("¿Qué estás estudiando?"))
+# Uso de print + input
+print("Estás estudiando: " + input("¿Qué estás estudiando?: "))
 
 print("Tu nombre es: " + input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: "))
 
-#proyecto lección 1 "Nombre de cerveza":
-print("Tu nombre de cerveza es:\n'" + input("Escribe un nombre de mujer: ") + " " + input("Escribe un adjetivo hermoso: ") + "'")
--- ejercicios/ex02_variablesYDatos.py
+++ ejercicios/ex02_variablesYDatos.py
@@ -13,7 +13,7 @@ curso = "Python"
 print("Estás tomando un curso de " + curso)
 
 num_entero = 10
-print(type(num_entero)) #Type imprime el tipo de dato
+print(type(num_entero)) #type imprime el tipo de dato
 
 num_decimal = 23.23 #los float van con punto, no con coma
 print(type(num_decimal))
@@ -22,25 +22,34 @@ num1 = 7.5
 num2 = 2.5
 print(type(num1 + num2))
 
-num1 = 7.5 #conversión explícita de float a int
-num1 = int(num1) #hacemos que el float 7.5 sea un int (7)
+#conversión explícita de float a int
+num1 = 7.5
+num1 = int(num1) #convertimos el float 7.5 a un int (7)
 print(num1)
 print(type(num1))
 
-num2 = 10 #conversión implícita de int a float
-num2 = 10 + 0.5 #hacemos que el int 10 sea un float (10.5)
+#conversión implícita de int a float
+num2 = 10
+num2 = 10 + 0.5 #si sumamos a un entero un float, se transformará a float
 print(num2)
 print(type(num2))
 
+#conversión explícita de int a float
+num3 = 1000
+print(num3, "es un entero")
+num3 = float(num3)
+print(num3, "pero ahora es lo transformo!")
+print("y ahora es: ", type(num3))
+
 num1 = "7.5"
 num2 = "10"
-print(num1 + num2) #Al ser strings, los contatena, no los suma
-print(float(num1) + int(num2)) #los convertimos a float y int para poder sumarlos
+print("concatenamos", num1 + num2) #Al ser strings, los contatena, no los suma
+print("Transformánsolos la suma es: ", float(num1) + int(num2)) #los convertimos a float e int para poder sumarlos
 
-#Uso de .format para contatenar string
+#Uso de .format ({}) para contatenar strings
 x = 7
 y = 3
-z = x + y #podemos crear otras variable para la suma, pero no es necesario
+z = x + y #podemos crear otras variables para la suma, pero no es necesario
 print("La suma de " + str(x) + " y " + str(y) + " es " + str(x + y)) #sin .format
 print("La suma de {} y {} es {}".format(x, y, x+y)) #usando .format
 print(f"La suma de {x} y {y} es {x+y}") #usando cadenas literales (f)
@@ -52,7 +61,8 @@ print(f"{x} elevado a {10} es {x**10}") #Potencias
 print(f"La raiz cuadrada de {x} es {x**0.5}") #Raiz Cuadrada
 
 #uso de round para redondear
-print(round(10/3, 2)) #redondeamos la división 10/3 dejando únnicamente 2 decimales
+print(round(10/3, 2)) #redondeamos la división 10/3 dejando únicamente 2 decimales
+
 valor = 10.676767
 print(round(valor)) #redondeamos el valor a su entero más cercano (11)
 print(round(5**0.5, 4)) #Raiz cuadrada de 5 dejando el resultado en 4 decimales
