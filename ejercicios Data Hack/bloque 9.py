aebfefa6ef9e5dcfd2afc74a1983b905b9e02787
jorgedediosb <jorgededios@icloud.com>
2023-10-20 19:20:42 +0200

    update bloque9

--- ejercicios Data Hack/bloque 9.py
+++ ejercicios Data Hack/bloque 9.py
@@ -3,8 +3,6 @@
 # 32. Escribir un programa que diga al usuario que ingrese una cadena.
 # El programa tiene que evaluar la cadena y decir cuántas letras mayúsculas tiene.
 
-"""
-
 print('EJERCICIO 32:')
 def cuantas_mayusculas(cadena):
     num_mayusc = 0
@@ -17,6 +15,7 @@ def cuantas_mayusculas(cadena):
 cadena_usuario = input('Ingresa un texto para contar las mayúsculas: ')
 resultado = cuantas_mayusculas(cadena_usuario)
 print(f'Tu texto tiene {resultado} mayúscula/s')
+
 
 # 33. Construir un pequeño programa que convierta números binarios en enteros.
 
@@ -33,6 +32,7 @@ def aDecimal(numeroBin):
 
 mi_decimal = 1010
 print(aDecimal(mi_decimal))
+
 
 # 34. Escribir un pequeño programa donde:
 # • Se ingresa el año en curso.
@@ -52,6 +52,7 @@ def cuantos_anos():
 
 cuantos_anos()
 
+
 # 35. Definir una tupla con 10 edades de personas.
 # Imprimir la cantidad de personas con edades superiores a 20.
 
@@ -67,13 +68,12 @@ mi_tupla = (15, 20, 25, 74, 19, 0)
 resultado = mayorA20(mi_tupla)
 print(f'en la tupla hay {resultado} persona/s mayores de 20')
 
-"""
 
 # 36. Definir una lista con un conjunto de nombres.
 # Imprimir la cantidad de comienzan con la letra a.
 # También se puede elegir al usuario la letra a buscar.
 
-print('\nEJERCICIO 35:')
+print('\nEJERCICIO 36:')
 
 print('Opción 1:')
 
@@ -107,3 +107,39 @@ def empiezanPor():
 
 cantidad = empiezanPor()
 print(f"La cantidad de nombres que comienzan con la letra ingresada es: {cantidad}")
+
+
+# 37. Crear una función contar_vocales(), que reciba una palabra
+# y cuente cuantas letras "a", "e", etc tiene y así hasta completar todas las vocales.
+# Se puede hacer que el usuario sea quien elija la palabra.
+
+print('\nEJERCICIO 37:')
+
+def contar_vocales(palabra):
+    palabra = palabra.lower()
+    vocales = "aeiou"
+    for x in vocales:
+        contador = 0
+        for i in palabra:
+            if i == x:
+                contador += 1
+        print("Hay %d %s." % (contador, x))
+
+mi_palabra = 'Jorge'
+print(contar_vocales(mi_palabra))
+
+# 38. Escriba una función es bisiesto() que determine si un año determinado es un año bisiesto.
+# Un año bisiesto es divisible por 4, pero no por 100. También es divisible por 400.
+
+print('\nEJERCICIO 38:')
+
+def es_bisiesto():
+    a = int(input("Escriba un año y le dire si es bisiesto: "))
+    if (a % 4 == 0) and (not(a % 100 == 0)):
+        print("El año", a, "es un año bisiesto porque es multiplo de 4")
+    elif a % 400 == 0:
+        print("El año", a, "es un año bisiesto porque es multiplo de 400")
+    else:
+        print("El año", a, "no es bisiesto")
+
+es_bisiesto()
