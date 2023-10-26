5baa6d2ec73a9f3e287729ce23af613e9bc43b9b
jorgedediosb <jorgededios@icloud.com>
2023-10-26 12:23:22 +0200

    update ../ejercicios_Udemy/ex03_listas.py

--- ejercicios/ejercicios_Udemy/ex03_listas.py
+++ ejercicios/ejercicios_Udemy/ex03_listas.py
@@ -1,5 +1,6 @@
 #Ejercicios lección 3 - Métodos con listas, Diccionarios, tuples y sets
 
+#type() nos dice el tipo de dato de una variable:
 mi_lista = [1, "hola", 55, True, 5.76]
 print(type(mi_lista))
 
@@ -18,46 +19,58 @@ dic = {'c1':'valor1', 'c2':'valor2', 'c3':'valor3'}
 print(type(dic))
 
 dic = {'c1':['a','b','c'],'c2':['d','e','f']}
-print(dic['c2'][1].upper()) #imprimimos el 2º elemento (e) de la 2ª clase (c2) y lo hacemos mayúscula
+#imprimimos el 2º elemento (e) de la 2ª clase (c2) y lo hacemos mayúscula
+print(dic['c2'][1].upper())
 
 
 mi_dict = {"valores_1":{"v1":3,"v2":6},"puntos":{"points1":9,"points2":[10,300,15]}}
-print(mi_dict["puntos"]["points2"][1]) #imprimimos '300'
+#imprimimos '300':
+print(mi_dict["puntos"]["points2"][1])
 
 mi_dic = {"nombre":"Karen", "apellido":"Jurgens", "edad":35, "ocupacion":"Periodista"}
-mi_dic["edad"] = 36 #cambiamos la edad
-mi_dic["ocupacion"] = "Editora" #cambiamos la ocupación
-mi_dic["pais"] = "Colombia" #añadimos 'pais'
+#cambiamos la edad:
+mi_dic["edad"] = 36
+#cambiamos la ocupación:
+mi_dic["ocupacion"] = "Editora" 
+#añadimos 'pais':
+mi_dic["pais"] = "Colombia"
 
-#TUPLES
+#TUPLAS
 mi_tuple = (1,2,3,4)
 print(type(mi_tuple))
 print(mi_tuple[2])
 
 mi_tupla = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
-print(mi_tupla.count(2)) #el mético count() me dice cuantos elementos hay repetidos de un elem que queramos
+#count() dice cuantos elementos hay repetidos:
+print(mi_tupla.count(2)) 
 
 mi_tupla = (1, 2, 3, 2, 3, 1, 3, 2)
-mi_lista = list(mi_tupla) #COnvertimos un tuple en una lista (o al revés)
+#Convertimos la tupla en una lista (o al revés):
+mi_lista = list(mi_tupla)
 print(type(mi_lista))
 
 mi_tupla = (1, 2, 3, 4)
-a,b,c,d = mi_tupla #Almacenamos los elementos de una tuple en varias variables distintas
+#Almacenamos los elementos de una tupla en varias variables distintas:
+a,b,c,d = mi_tupla
 print(a,b,c,d)
 
 #SETS
 mi_set_1 = {1, 2, "tres", "cuatro"}
 mi_set_2 = {"tres", 4, 5}
-mi_set_3 = mi_set_1.union(mi_set_2) #une el set1  y el set2 en un tercer set (set3) usando él método .union()
+#set1 y el set2 se une en un tercer set (set3) usando él método .union():
+mi_set_3 = mi_set_1.union(mi_set_2)
 print(mi_set_3)
-mi_set_3 = mi_set_1 | mi_set_2 #También se pueden unir con el operador |
+#También se pueden unir con el operador |:
+mi_set_3 = mi_set_1 | mi_set_2
 print(mi_set_3)
 
 sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}
-print(sorteo.pop()) #pop() elimina al azar un elemento del set
+#pop() elimina al azar un elemento del set:
+print(sorteo.pop())
 
 sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}
-sorteo.add("Damián") #Añadimos un elemento al set con add()
+#add() añade un elemento al set:
+sorteo.add("Damián") 
 print(sorteo)
 
 #BOLEANOS, devuelve el resultado booleano True o False
@@ -69,8 +82,10 @@ x = bool(15 > 7)
 print(x)
 
 lista = [1,2,3,4]
-control = 5 not in lista #preguntamos si 5 está en la lista
+#preguntamos si 5 está en la lista:
+control = 5 not in lista
 print(control) #=> False
 
-x = 25**0.5 == 5 #¿Es la raiz cuadrada de 25 igual a 5?
+#¿Es la raiz cuadrada de 25 igual a 5?
+x = 25**0.5 == 5
 print(x) # => True
