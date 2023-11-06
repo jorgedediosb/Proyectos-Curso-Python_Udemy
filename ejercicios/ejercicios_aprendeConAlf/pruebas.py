162e82b807adfffd5615d7ec00ccd7470f544d40
jorgedediosb <jorgededios@icloud.com>
2023-11-06 21:07:07 +0100

    add ejercicios FreeCodeCamp

--- ejercicios/ejercicios_aprendeConAlf/pruebas.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-titanic = pd.read_csv('dataSets/titanic.csv', index_col=0)
-print('-------------')
-print(titanic['Survived'].value_counts())
-print(titanic['Pclass'].value_counts())
